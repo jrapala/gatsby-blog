df29e3536c9762faf0b3366493723c29b0ea0464
Juliette Rapala <juliette@eventric.com>
2019-08-05 14:01:55 -0500

    Add title and description metadata using graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,71 +1,81 @@
 import React, { Fragment } from 'react'
 import { Global, css } from '@emotion/core'
+import Helmet from 'react-helmet'
 import Header from './header'
+import useSiteMetadata from '../hooks/use-sitemetadata'
 
-const Layout = ({ children }) => (
-	<Fragment>
-		<Global
-			styles={css`
-				* {
-					box-sizing: border-box;
-					margin: 0;
-				}
+const Layout = ({ children }) => {
+	const { title, description } = useSiteMetadata()
 
-				/* Every sibling selector.. */
-				* + * {
-					margin-top: 1rem;
-				}
-
-				html,
-				body {
-					margin: 0;
-					color: #555;
-					font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
-						Arial, sans-serif;
-					font-size: 18px;
-					line-height: 1.4;
-
-					/* remove margin for main div that Gatsby mounts into */
-					> div {
-						margin-top: 0;
+	return (
+		<Fragment>
+			<Global
+				styles={css`
+					* {
+						box-sizing: border-box;
+						margin: 0;
 					}
-				}
 
-				h1,
-				h2,
-				h3,
-				h4,
-				h5,
-				h6 {
-					color: #222;
-					line-height: 1.1;
-
-					/* Anything that comes after a heading... */
-					+ * {
-						margin-top: 0.5rem;
+					/* Every sibling selector.. */
+					* + * {
+						margin-top: 1rem;
 					}
-				}
 
-				strong {
-					color: #222;
-				}
+					html,
+					body {
+						margin: 0;
+						color: #555;
+						font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+							Helvetica, Arial, sans-serif;
+						font-size: 18px;
+						line-height: 1.4;
 
-				li {
-					margin-top: 0.25rem;
-				}
-			`}
-		/>
-		<Header />
-		<main
-			css={css`
-				margin: 2rem auto 4em;
-				max-width: 90vw;
-				width: 550px;
-			`}
-		>
-			{children}
-		</main>
-	</Fragment>
-)
+						/* remove margin for main div that Gatsby mounts into */
+						> div {
+							margin-top: 0;
+						}
+					}
 
+					h1,
+					h2,
+					h3,
+					h4,
+					h5,
+					h6 {
+						color: #222;
+						line-height: 1.1;
+
+						/* Anything that comes after a heading... */
+						+ * {
+							margin-top: 0.5rem;
+						}
+					}
+
+					strong {
+						color: #222;
+					}
+
+					li {
+						margin-top: 0.25rem;
+					}
+				`}
+			/>
+			<Helmet>
+				<html lang="en" />
+				<title>{title}</title>
+				<meta name="description" content={description} />
+			</Helmet>
+			<Header />
+			<main
+				css={css`
+					margin: 2rem auto 4em;
+					max-width: 90vw;
+					width: 550px;
+				`}
+			>
+				{children}
+			</main>
+		</Fragment>
+	)
+}
 export default Layout
