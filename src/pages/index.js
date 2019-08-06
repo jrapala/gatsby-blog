4f43b6a2b0fe9687ca872ffed037bf13008d500f
Juliette Rapala <juliette@eventric.com>
2019-08-05 20:25:15 -0500

    Add hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Hero from '../components/hero'
 import PostPreview from '../components/post-preview'
 import usePosts from '../hooks/use-posts'
 
@@ -8,15 +9,17 @@ export default () => {
 	const posts = usePosts()
 
 	return (
-		<Layout>
-			<h1>Home</h1>
-			<p>Hello Minnesota!</p>
-			<Link to="/about">Learn about me &rarr;</Link>
-			<h2>Read my blog</h2>
-			{posts.map(post => (
-				// <pre>{JSON.stringify(post, null, 4)}</pre>
-				<PostPreview key={post.slug} post={post} />
-			))}
-		</Layout>
+		<Fragment>
+			<Hero />
+			<Layout>
+				<h1>Home</h1>
+				<p>Hello Minnesota!</p>
+				<Link to="/about">Learn about me &rarr;</Link>
+				<h2>Read my blog</h2>
+				{posts.map(post => (
+					<PostPreview key={post.slug} post={post} />
+				))}
+			</Layout>
+		</Fragment>
 	)
 }
