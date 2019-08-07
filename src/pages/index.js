38ce6d80408a2738c1fbeba838c30f111f6031af
Juliette Rapala <juliette@eventric.com>
2019-08-07 17:27:11 -0500

    Add Instagram images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+import Insta from '../components/insta'
 import PostPreview from '../components/post-preview'
 import usePosts from '../hooks/use-posts'
 
@@ -19,6 +20,7 @@ export default () => {
 				{posts.map(post => (
 					<PostPreview key={post.slug} post={post} />
 				))}
+				<Insta />
 			</Layout>
 		</Fragment>
 	)
